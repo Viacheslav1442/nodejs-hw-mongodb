e4b0ed8bd35f30f6c0da80b77228cbf69b456edf
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-10-09 10:48:47 +0300

    js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,21 +1,9 @@
 import express from "express";
-import Joi from "joi";
 import { register, login, refresh, logout } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { registerSchema, loginSchema } from "../validation/authValidation.js";
 
 const router = express.Router();
-
-const registerSchema = Joi.object({
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-});
-
-const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-});
 
 router.post("/register", validateBody(registerSchema), register);
 router.post("/login", validateBody(loginSchema), login);
