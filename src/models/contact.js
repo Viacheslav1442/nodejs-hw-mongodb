7c82ea6fb5524b3af30d73a909a7ae9da5ca6162
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-10-10 15:09:32 +0300

    js

--- src/models/contact.js
+++ src/models/contact.js
@@ -4,7 +4,7 @@ const contactSchema = new Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
-        phone: { type: String, required: true },
+        phoneNumber: { type: String, required: true },
         favorite: { type: Boolean, default: false },
         contactType: {
             type: String,
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,19 +2,25 @@ import express from "express";
 import contactsController from "../controllers/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { authenticate } from "../middlewares/authenticate.js";
-import {
-    createContactSchema,
-    updateContactSchema,
-} from "../validation/contactValidation.js";
+import { createContactSchema, updateContactSchema } from "../validation/contactValidation.js";
 
 const router = express.Router();
 
 router.use(authenticate);
 
+// Отримати всі контакти
 router.get("/", contactsController.getAllContacts);
+
+// Отримати один контакт
 router.get("/:id", contactsController.getContactById);
+
+// Створити контакт
 router.post("/", validateBody(createContactSchema), contactsController.createContact);
+
+// Оновити контакт
 router.patch("/:id", validateBody(updateContactSchema), contactsController.updateContact);
+
+// Видалити контакт
 router.delete("/:id", contactsController.deleteContact);
 
 export default router;
