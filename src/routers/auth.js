91da7e3ab45e695151da73e7fb1a35c5c1a337dc
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-10-13 11:57:10 +0300

    js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,24 +1,39 @@
 import express from "express";
-import { register, login, refresh, logout } from "../controllers/auth.js";
+import {
+    register,
+    login,
+    refresh,
+    logout,
+    resetPasswordController,
+    sendResetEmailController
+} from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { registerSchema, loginSchema } from "../validation/authValidation.js";
-import { resetPasswordController } from "../controllers/auth.js";
 import { resetPasswordSchema } from "../validation/resetPwdValidation.js";
 import { sendResetEmailSchema } from "../validation/resetEmailValidation.js";
 
 const router = express.Router();
 
+// ------------------ РЕЄСТРАЦІЯ ------------------
 router.post("/register", validateBody(registerSchema), register);
+
+// ------------------ ЛОГІН ------------------
 router.post("/login", validateBody(loginSchema), login);
+
+// ------------------ REFRESH ------------------
 router.post("/refresh", refresh);
+
+// ------------------ LOGOUT ------------------
 router.post("/logout", logout);
 
+// ------------------ SEND RESET EMAIL ------------------
 router.post(
     "/send-reset-email",
     validateBody(sendResetEmailSchema),
     sendResetEmailController
 );
 
+// ------------------ RESET PASSWORD ------------------
 router.post(
     "/reset-pwd",
     validateBody(resetPasswordSchema),
