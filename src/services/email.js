d114bbc8068138ad911bcc3567b640fcc4c88d3f
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-10-15 19:55:21 +0300

    js

--- src/services/email.js
+++ src/services/email.js
@@ -31,9 +31,6 @@ export const sendResetEmail = async (to, link) => {
         console.error("❌ Email send error:", error.message);
         throw new Error("Failed to send email");
     }
-    console.log("📧 Trying to send email to:", to);
-    console.log("SMTP HOST:", process.env.SMTP_HOST);
-    console.log("SMTP USER:", process.env.SMTP_USER);
 
 };
 
