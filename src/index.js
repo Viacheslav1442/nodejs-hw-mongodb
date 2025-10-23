2e183b9169d0aa34f4f23a2c478d6f0b33b33589
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-10-23 16:12:29 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import "dotenv/config";
 import { initMongoConnection } from "./db/initMongoConnection.js";
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-import contactsRouter from "./routers/contacts.js";
-import authRouter from "./routers/auth.js";
-import { authenticate } from "./middlewares/authenticate.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
-import cookieParser from "cookie-parser";
+import { setupServer } from "./server.js";
 
 const startServer = async () => {
-    await initMongoConnection();
-
-    const app = express();
-
-    app.use(cors());
-    app.use(pino());
-    app.use(express.json());
-    app.use(cookieParser());
-
-
-    app.use("/auth", authRouter);
-
-
-    app.use("/contacts", authenticate, contactsRouter);
-
-    // 404
-    app.use((req, res) => res.status(404).json({ message: "Not found" }));
-
-
-    app.use(errorHandler);
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    await initMongoConnection(); // підключення до MongoDB
+    setupServer(); // запуск сервера
 };
 
 startServer();
